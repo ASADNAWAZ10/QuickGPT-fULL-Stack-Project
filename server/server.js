b9e7ffe514fa450e4ea21715417004fef6f1c89b
ASADNAWAZ10 <asadnawazasadnawaz211@gmail.com>
2026-04-16 21:07:44 -0600

    error fix in server.js

--- server/server.js
+++ server/server.js
@@ -27,6 +27,10 @@ app.use('/api/credit', creditRouter )
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, ()=>{
-    console.log(`server is runing at ${PORT}`)
-})
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
+
+export default app;
